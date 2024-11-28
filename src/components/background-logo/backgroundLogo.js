95e206028c9682a25bb9c7b1050edef653bd8bdd
immprada <im.mprada@gmail.com>
2024-11-27 21:02:58 -0500

    add styles

---
+++ src/components/background-logo/backgroundLogo.js
@@ -0,0 +1,10 @@
+import './styles.scss'
+import patternCurve from '../../images/pattern-curve.svg'
+
+const backgroundLogo = `
+  <div class="background-logo">
+    <img src="${patternCurve}" alt="" />
+  </div>
+`
+
+export default backgroundLogo;

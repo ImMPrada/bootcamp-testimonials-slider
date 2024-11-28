1f6bafe368fe5bb86c17d0d0d0223ffcb1a66ca9
miguel <26731448+ImMPrada@users.noreply.github.com>
2024-11-28 10:41:23 -0500

    Add Initial setup (#1)
    * add vite
    
    * add resources
    
    * add vite
    
    * add SASS
    
    * add assets
    
    * add styles basis
    
    * add styles
    
    * add eslint
    
    * add eslint

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './styles/main.scss';
+import { setupCounter } from './counter.js';
+import backgroundLogo from './components/background-logo/backgroundLogo.js';
+
+document.querySelector('#app').innerHTML = `
+  <div class="testimonials">
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+  ${backgroundLogo}
+`;
+
+setupCounter(document.querySelector('#counter'));

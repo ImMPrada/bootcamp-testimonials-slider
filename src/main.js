4f3fdbe6015ef4fc871b1686af2ddc22b22faa4e
immprada <im.mprada@gmail.com>
2024-12-02 09:16:46 -0500

    main js now imports and initializes state and assigns buttons event listeners

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import './styles/main.scss';
 import testimonials from './components/testimonials/testimonials.js';
+import { state } from './scripts/state.js';
+import { buildAction, addListenerToButton, renderTestimonial } from './scripts/actions.js';
 
 document.querySelector('#app').innerHTML = testimonials();
+renderTestimonial(state);
+
+const changeTestimonialsIndex = buildAction(state);
+
+addListenerToButton(state, 'prevButton', () => {
+  changeTestimonialsIndex(-1);
+  console.log(state);
+  renderTestimonial(state);
+});
+
+addListenerToButton(state, 'nextButton', () => {
+  changeTestimonialsIndex(1);
+  console.log(state);
+  renderTestimonial(state);
+});
